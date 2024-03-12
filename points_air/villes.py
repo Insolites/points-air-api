4f0cd93be9862dd891618ff37107eea6afd1ae90
David Huggins-Daines <dhd@ecolingui.ca>
2024-03-12 10:33:42 -0400

    feat: trouver Laval sur une carte

--- points_air/villes.py
+++ points_air/villes.py
@@ -4,13 +4,37 @@ Ce module regroupe les fonctions pour télécharger les géométries des
 villes, ainsi que pour localiser des emplacements à l'intérieur des villes.
 """
 
+import argparse
+import asyncio
+import json
 import urllib
 
+from shapely import Point
+from shapely.geometry import shape
+from pathlib import Path
 from pydantic import BaseModel
 from typing import Optional
 from httpx import AsyncClient
 
 CLIENT = AsyncClient()
+VILLES = """
+Rimouski
+Repentigny
+Sainte-Adèle
+Montréal
+Laval
+Gatineau
+Sherbrooke
+""".strip().split()
+THISDIR = Path(__file__).parent
+VILLEGONS = {}
+try:
+    with open(THISDIR / "villes.json", "rt") as infh:
+        feats = json.load(infh)
+    for v, f in feats.items():
+        VILLEGONS[v] = shape(f["geometry"])
+except json.JSONDecodeError:  # Si on reconstruit le JSON..
+    pass
 
 
 def icherche_url(ville: str) -> str:
@@ -38,7 +62,28 @@ class Ville(BaseModel):
     """
     Une ville de compétition.
     """
+    nom: str
+
     @classmethod
-    def from_wgs84(self, latitude: float, longitude: float) -> "Ville":
-        # TODO
-        return Ville()
+    def from_wgs84(self, latitude: float, longitude: float) -> Optional["Ville"]:
+        p = Point(longitude, latitude)
+        for v, g in VILLEGONS.items():
+            if g.contains(p):
+                return Ville(nom=v)
+        return None
+
+
+async def async_main(args: argparse.Namespace):
+    ville_dict = {}
+    for v in args.villes:
+        fc = await ville_json(v)
+        ville_dict[v] = fc["features"][0]
+    print(json.dumps(ville_dict, indent=2, ensure_ascii=False))
+
+
+def main():
+    """Télécharger GeoJSON pour toutes les villes."""
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("villes", help="Noms des villes", nargs="*", default=VILLES)
+    args = parser.parse_args()
+    asyncio.run(async_main(args))
--- tests/test_villes.py
+++ tests/test_villes.py
@@ -1,11 +1,11 @@
 import pytest
 
-from points_air.villes import ville_json
+from points_air.villes import ville_json, Ville
 
 
 @pytest.mark.anyio
 async def test_laval():
-    """Trouver Laval sur une carte"""
+    """Trouver Laval avec iCherche"""
     geom = await ville_json("Laval")
     # inutile de faire assert parce que ça va planter tout de suite si
     # ces membres sont absents!
@@ -14,3 +14,11 @@ async def test_laval():
     geometry = laval["geometry"]
     assert geometry["type"] == "MultiPolygon"
     assert geometry["coordinates"]
+
+
+def test_find_laval():
+    """Trouver Laval sur une carte"""
+    quelque_part_a_laval = [45.617610, -73.764797]
+    laval = Ville.from_wgs84(*quelque_part_a_laval)
+    # On se trouve bel et bien à Laval
+    assert laval.nom == "Laval"
